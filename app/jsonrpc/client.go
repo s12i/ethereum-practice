8231a4132321d7c0ac10dc9da3079dfaaa791e89
Wuhsien <wuhsienyu@gmail.com>
2018-03-11 15:30:42 +0800

    add JSON RPC client to local geth

---
+++ app/jsonrpc/client.go
@@ -0,0 +1,38 @@
+package jsonrpc
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+
+	simplejson "github.com/bitly/go-simplejson"
+	"github.com/gorilla/rpc/v2/json2"
+)
+
+const (
+	rpcUrl  string = "localhost"
+	rpcPort int    = 8545
+)
+
+func ClientRequest(method string, params interface{}) *simplejson.Json {
+	data, _ := json2.EncodeClientRequest(method, params)
+	request, _ := http.NewRequest("POST", fmt.Sprintf("http://%s:%d", rpcUrl, rpcPort), bytes.NewBuffer(data))
+
+	request.Header.Set("Content-Type", "application/json")
+	reply, err := http.DefaultClient.Do(request)
+	defer reply.Body.Close()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(reply.Body)
+	response, err := simplejson.NewJson(buffer.Bytes())
+	if err != nil {
+		fmt.Println("parse JSON fail")
+	}
+
+	return response
+}
