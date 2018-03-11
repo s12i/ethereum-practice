5c99706b6d38aba62148306e60fe572e104fbd28
Wuhsien <wuhsienyu@gmail.com>
2018-03-11 17:37:21 +0800

    add comments for /node

--- app/admin.go
+++ app/admin.go
@@ -7,6 +7,10 @@ import (
 	"github.com/s12i/maicon-fullstack-test/app/jsonrpc"
 )
 
+/*
+GetNodeInfo - 取得節點資訊
+Go-Ethereum JSON-RPC API：https://github.com/ethereum/go-ethereum/wiki/Management-APIs#admin_nodeinfo
+*/
 func GetNodeInfo(context *gin.Context) {
 	response := jsonrpc.ClientRequest("admin_nodeInfo", nil)
 
